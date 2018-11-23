dcb0640f3cf714b16437bfe30bec45ee2b37cec8
jArecki444 <arkadiusz.cichurski@gmail.com>
2018-11-23 15:00:23 +0100

    Dodanie authService i drobne poprawki w nav component - implementacja logowania

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {}
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(
+      next => {
+        console.log('Zalogowano pomyslnie');
+      },
+      error => {
+        console.log('Problem z logowaniem');
+      }
+    );
   }
 
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
 }
