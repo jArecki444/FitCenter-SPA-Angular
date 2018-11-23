797d61aa5c55107e88185d1f583c416678a3d69c
jArecki444 <arkadiusz.cichurski@gmail.com>
2018-11-23 15:53:45 +0100

    Stworzenie komponentu register i home (działająca rejestracja)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ValueComponent,
-    NavComponent
+    NavComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
